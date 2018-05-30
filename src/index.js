ae01ca467a07270d37e3e564e9da7d04f0ab2f26
KirillPavlovBsu <34164886+KirillPavlovBsu@users.noreply.github.com>
2018-05-31 01:27:08 +0300

    Meals 0.1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import React from 'react'
-import {render} from 'react-dom'
-import App from './components/App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './Components/App';
+import registerServiceWorker from './registerServiceWorker';
 
-render(<App/>, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
---
+++ src/jsonOfMeals.js
@@ -0,0 +1,133 @@
+import faker from 'faker'
+
+export default [
+{   
+    "meal": "Pizza",
+    "name": "Barbekku",
+    "profile": "National",
+    "restaurant": "KFC",
+    "comments": faker.random.number(10)},
+{   
+    "meal": "Pizza",
+    "name": "Calcone",
+    "profile": "American",
+    "restaurant": "BK",
+    "comments": faker.random.number(10)},
+{   
+    "meal": "Pizza",
+    "name": "Stromboli",
+    "profile": "Asian",
+    "restaurant": "Dominos",
+    "comments": faker.random.number(10)},
+{   
+    "meal": "Pizza",
+    "name": "Pancerotto",
+    "profile": "East",
+    "restaurant": "MCD",
+    "comments": faker.random.number(10)},
+{   
+    "meal": "Kebab",
+    "name": "Cheesy",
+    "profile": "Italian",
+    "restaurant": "Texas",
+    "comments": faker.random.number(10)},
+{   
+    "meal": "Kebab",
+    "name": "King",
+    "profile": "Exotic",
+    "restaurant": "BK",
+    "comments": faker.random.number(10)},
+{   
+    "meal": "Kebab",
+    "name": "Unique",
+    "profile": "National",
+    "restaurant": "Dominos",
+    "comments": faker.random.number(10)},
+{   
+    "meal": "Soup",
+    "name": "Borsch",
+    "profile": "American",
+    "restaurant": "Texas",
+    "comments": faker.random.number(10)},
+{   
+    "meal": "Soup",
+    "name": "Harcho",
+    "profile": "Asian",
+    "restaurant": "MCD",
+    "comments": faker.random.number(10)},
+{   
+    "meal": "Soup",
+    "name": "Okroshka",
+    "profile": "East",
+    "restaurant": "KFC",
+    "comments": faker.random.number(10)},
+{   
+    "meal": "Sushi",
+    "name": "California",
+    "profile": "Italian",
+    "restaurant": "BK",
+    "comments": faker.random.number(10)},
+{   
+    "meal": "Sushi",
+    "name": "Missuri",
+    "profile": "Exotic",
+    "restaurant": "MCD",
+    "comments": faker.random.number(10)},
+{   
+    "meal": "Sushi",
+    "name": "Misissippi",
+    "profile": "National",
+    "restaurant": "Dominos",
+    "comments": faker.random.number(10)},
+{   
+    "meal": "Sushi",
+    "name": "Filadelfia",
+    "profile": "American",
+    "restaurant": "BK",
+    "comments": faker.random.number(10)},
+{
+    "Restname":"KFC",
+    "criteria": {
+        "segmentcost":"low",
+        "service":"bad",
+        "online":"on",
+        "location":"nearby"
+    },
+    "rank": 10},
+{
+    "Restname":"BK",
+    "criteria": {
+        "segmentcost":"mid",
+        "service":"norm",
+        "online":"off",
+        "location":"far"
+    },
+    "rank": 89},
+{
+    "Restname":"MCD",
+    "criteria": {
+        "segmentcost":"high",
+        "service":"good",
+        "online":"on",
+        "location":"nearby"
+    },
+    "rank": 40},
+{
+    "Restname":"Texas",
+    "criteria": {
+        "segmentcost":"mid",
+        "service":"norm",
+        "online":"off",
+        "location":"far"
+    },
+    "rank": 21},
+{
+    "Restname":"Dominos",
+    "criteria": {
+        "segmentcost":"low",
+        "service":"good",
+        "online":"on",
+        "location":"far"
+    },
+    "rank": 73},
+]
